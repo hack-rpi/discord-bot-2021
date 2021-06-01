e590ed96e256a960817f8549f50331815ded2c8e
charles <46334090+charlestian23@users.noreply.github.com>
2021-06-01 19:03:31 -0400

    Encoding and decoding json information
    Changed encode() and decode() to take in and return json compatible information

--- base64_encoding.py
+++ base64_encoding.py
@@ -1,15 +1,19 @@
 import base64
+import json
 
 
-def encode(original):
+# Takes in a json object
+def encode(json_object):
+    original = json.dumps(json_object)
     message_bytes = original.encode("ascii")
     base64_bytes = base64.b64encode(message_bytes)
     base64_message = base64_bytes.decode("ascii")
     return base64_message
 
 
+# Takes in a string
 def decode(code):
     base64_bytes = code.encode("ascii")
     message_bytes = base64.b64decode(base64_bytes)
     message = message_bytes.decode("ascii")
-    return message
+    return json.loads(message)
