f3331b355b57e365a4a294d66eb75be18de69dd0
jaw12346 <jaw12346@gmail.com>
2021-06-29 19:28:35 -0400

    Added emoji validation, changed format of type verification for some embed variables, added emoji library

--- error_checking.py
+++ error_checking.py
@@ -1,22 +1,40 @@
 import discord
+import emoji
 
 
-def is_emoji(emoji):
-    try:
-        if not isinstance(emoji, str) and emoji[0] != '<' and emoji[-1] != '>':
-            return False
-        lis = emoji[2:-1].split(":")
-        if not isinstance(lis[0], str):
-            return False
-        if not isinstance(lis[1], str) and not lis[1].isnumeric():
-            return False
-        return True
-    except IndexError:
-        return False
+def check_emoji(_emoji, bot):
+    custom_emoji = False
+    if len(_emoji) > 1 and '<' == _emoji[0] and '>' == _emoji[-1] and ':' in _emoji:  # Custom discord emoji detected
+        # Unicode emojis only have ':' no '<' '>'
+        emote_name = _emoji.split(':')[1]  # Retrieve the emoji name
+        for server in bot.guilds:  # Search all servers the bot is in
+            for emote in server.emojis:  # Search all emojis in all servers the bot is in
+                if emote.name == emote_name and discord.Emoji.is_usable(emote):
+                    # Discord bots have Nitro privs for emojis -- Can use custom emojis from other servers
+                        return True, emote  # Bot is able to use the custom emoji
+        return False, "Incompatible"  # Bot is unable to use the provided emoji
+
+    emoji_name = emoji.demojize(_emoji)  # Generate Unicode emoji name
+    if emoji_name == _emoji:  # Custom emojis will not demojize
+        return False, "Incompatible"  # Bot is unable to use the provided emoji
+    else:  # Unicode emoji detected
+        return True, _emoji
 
 
-def embed_error_check(channel_category, custom_ticket, user_reaction, text):
-    if not (isinstance(channel_category, str) and isinstance(custom_ticket, str) and is_emoji(user_reaction) and \
-            isinstance(text, str)):
-        return False
-    return True
+def embed_error_check(channel_category, custom_ticket, user_reaction, text, bot):
+    channel_string = isinstance(channel_category, str)
+    ticket_string = isinstance(custom_ticket, str)
+    text_string = isinstance(text, str)
+
+    is_emoji, emoji_response = check_emoji(user_reaction, bot)
+
+    if not channel_string:
+        return True, "channel string"
+    elif not ticket_string:
+        return True, "ticket string"
+    elif not text_string:
+        return True, "text string"
+    elif not is_emoji:
+        return True, "emoji"
+    else:  # No errors
+        return False, ""
