1f8c8cbed82177ae8292f43c1db244816f29eb57
Charles <charles.tian23@gmail.com>
2021-08-03 16:18:21 -0400

    Added ticket creation confirmation
    Confirms that a ticket has been created for the user with a temporary response message from the bot

--- buttons.py
+++ buttons.py
@@ -14,6 +14,9 @@ class TicketCreationButton(discord.ui.Button["ticket_creation"]):
         super().__init__(label="OPEN TICKET", style=discord.ButtonStyle.blurple, custom_id="ticket_creation_button")
 
     async def callback(self, interaction: discord.Interaction):
+        interaction_response_embed = discord.Embed(description="A new ticket has been opened for you. Please check your"
+                                                               " list of channels to find it.")
+        await interaction.response.send_message(embed=interaction_response_embed, ephemeral=True)
         await channel_actions.create_help_channel(self, interaction)
 
 
